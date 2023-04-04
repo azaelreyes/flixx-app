f49d8e98d99792cfb891a700094113bd5fa0075f
azaelreyes <azaelreyes20.ar@gmail.com>
2023-04-04 13:41:27 -0700

    added feature to show movie details from API for each movie.

--- js/script.js
+++ js/script.js
@@ -61,6 +61,62 @@ function hideSpinner(){
     })
 }
 
+//Display Movie Details
+async function displayMovieDetails(){
+    const movieId = window.location.search.split('=')[1];
+    const movie = await fetchAPIData(`movie/${movieId}`);
+    const div = document.createElement('div');
+    div.innerHTML=`
+    <div class="details-top">
+    <div>
+    ${
+        movie.poster_path
+          ? `<img
+        src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+        class="card-img-top"
+        alt="${movie.title}"
+      />`
+          : `<img
+      src="../images/no-image.jpg"
+      class="card-img-top"
+      alt="${movie.title}"
+    />`
+      }
+    </div>
+    <div>
+      <h2>${movie.title}</h2>
+      <p>
+        <i class="fas fa-star text-primary"></i>
+        ${movie.vote_average.toFixed(1)} / 10
+      </p>
+      <p class="text-muted">Release Date: ${movie.release_date}</p>
+      <p>
+      ${movie.overview}
+      </p>
+      <h5>Genres</h5>
+      <ul class="list-group">
+        ${movie.genres.map((genre)=>`<li>${genre.name}</li>`).join('')}
+      </ul>
+      <a href="${movie.homepage}" target="_blank" class="btn">Visit Movie Homepage</a>
+    </div>
+  </div>
+
+  <div class="details-bottom">
+    <h2>Movie Info</h2>
+    <ul>
+      <li><span class="text-secondary">Budget:</span> $${addCommasToNumber(movie.budget)}</li>
+      <li><span class="text-secondary">Revenue:</span> $${addCommasToNumber(movie.revenue)}</li>
+      <li><span class="text-secondary">Runtime:</span> ${movie.runtime} minutes</li>
+      <li><span class="text-secondary">Status:</span> ${movie.status}</li>
+    </ul>
+    <h4>Production Companies</h4>
+    <div class="list-group">
+    ${movie.production_companies.map((company) => `<span>${company.name}</span>`).join(', ')}
+    </div>
+  </div>`;
+  document.querySelector('#movie-details').appendChild(div);
+
+}
 //Display 20 most popular tv shows
 async function displayPopularShows(){
     //the {} around results changes the object to just give arrays of movie objects.
@@ -107,6 +163,9 @@ function highlightActiveLink(){
         }
     });
 };
+function addCommasToNumber(number){
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
 
 //Init App
 function init (){
@@ -123,7 +182,7 @@ function init (){
             console.log('Tv Details');
             break;
         case '/movie-details.html' :
-            console.log('Movie Details');
+            displayMovieDetails();
             break;
         case '/search.html' :
                 console.log('Search');
