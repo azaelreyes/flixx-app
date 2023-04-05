2ddb2643e16c2233afb44d6cc723358ce7b9482f
azaelreyes <azaelreyes20.ar@gmail.com>
2023-04-05 08:42:14 -0700

    added functions to display movie and show details and backdrop images. Used Swiper API to display now playign movies on slides.

--- js/script.js
+++ js/script.js
@@ -10,6 +10,7 @@ async function fetchAPIData(endpoint){
     const API_KEY = '046cbad019d15b8e27aaba530c59eb0e';
     const API_URL = 'https://api.themoviedb.org/3/';
     showSpinner();
+    //https://api.themoviedb.org/3/   ENDPOINT    ?api_key=046cbad019d15b8e27aaba530c59eb0e&language=en-US
     const response =  await fetch(`${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`);
     const data = await response.json();
     hideSpinner()
@@ -24,12 +25,12 @@ function hideSpinner(){
 }
 
 
-
+//Display 20 most popular Movies
  async function displayPopularMovies(){
     //the {} around results changes the object to just give arrays of movie objects.
     // without it, it would give a lot more data other than results.
     const {results} =  await fetchAPIData('movie/popular');
-    //the async/await is here becuase it has to wait on fetchAPIData
+    //the async/await is here because it has to wait on fetchAPIData
 
     results.forEach((movie)=>{
         const div = document.createElement('div');
@@ -60,11 +61,14 @@ function hideSpinner(){
         document.querySelector('#popular-movies').appendChild(div);
     })
 }
-
 //Display Movie Details
 async function displayMovieDetails(){
     const movieId = window.location.search.split('=')[1];
     const movie = await fetchAPIData(`movie/${movieId}`);
+
+    //Overlay For Background Image
+    displayBackgroundImage('movie', movie.backdrop_path);
+
     const div = document.createElement('div');
     div.innerHTML=`
     <div class="details-top">
@@ -116,13 +120,35 @@ async function displayMovieDetails(){
   </div>`;
   document.querySelector('#movie-details').appendChild(div);
 
+
 }
+//Display Backdrop on Details Pages
+function displayBackgroundImage(type , backgroundPath){
+  const overlayDiv= document.createElement('div');
+  overlayDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${backgroundPath})`;
+  overlayDiv.style.backgroundSize = 'cover';
+  overlayDiv.style.backgroundPosition = 'center';
+  overlayDiv.style.backgroundRepeat = 'no-repeat';
+  overlayDiv.style.height = '100vh';
+  overlayDiv.style.width = '100vw';
+  overlayDiv.style.position = 'absolute';
+  overlayDiv.style.top= '0';
+  overlayDiv.style.left = '0';
+  overlayDiv.style.zIndex = '-1';
+  overlayDiv.style.opacity = '0.1'
+  if(type === 'movie'){
+    document.querySelector('#movie-details').appendChild(overlayDiv);
+  }else {
+    document.querySelector('#show-details').appendChild(overlayDiv);
+  }
+}
+
 //Display 20 most popular tv shows
 async function displayPopularShows(){
     //the {} around results changes the object to just give arrays of movie objects.
     // without it, it would give a lot more data other than results.
     const {results} =  await fetchAPIData('tv/popular');
-    //the async/await is here becuase it has to wait on fetchAPIData
+    //the async/await is here because it has to wait on fetchAPIData
 
     results.forEach((show)=>{
         const div = document.createElement('div');
@@ -153,6 +179,110 @@ async function displayPopularShows(){
         document.querySelector('#popular-shows').appendChild(div);
     })
 }
+//Display Show Details
+async function displayShowDetails(){
+  const showId = window.location.search.split('=')[1];
+  const show = await fetchAPIData(`tv/${showId}`);
+
+  //Overlay For Background Image
+  displayBackgroundImage('tv', show.backdrop_path);
+
+  const div = document.createElement('div');
+  div.innerHTML=`
+  <div class="details-top">
+  <div>
+  ${
+      show.poster_path
+        ? `<img
+      src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+      class="card-img-top"
+      alt="${show.name}"
+    />`
+        : `<img
+    src="../images/no-image.jpg"
+    class="card-img-top"
+    alt="${show.name}"
+  />`
+    }
+  </div>
+  <div>
+    <h2>${show.name}</h2>
+    <p>
+      <i class="fas fa-star text-primary"></i>
+      ${show.vote_average.toFixed(1)} / 10
+    </p>
+    <p class="text-muted">Last Air Date: ${show.last_air_date}</p>
+    <p>
+    ${show.overview}
+    </p>
+    <h5>Genres</h5>
+    <ul class="list-group">
+      ${show.genres.map((genre)=>`<li>${genre.name}</li>`).join('')}
+    </ul>
+    <a href="${show.homepage}" target="_blank" class="btn">Visit show Homepage</a>
+  </div>
+</div>
+
+<div class="details-bottom">
+  <h2>Show Info</h2>
+  <ul>
+    <li><span class="text-secondary">Number Of Episodes:</span> $${show.number_of_episodes}</li>
+    <li><span class="text-secondary">Last Episode To Air:</span> $${show.last_air_date}</li>
+    <li><span class="text-secondary">Status:</span> ${show.status}</li>
+  </ul>
+  <h4>Production Companies</h4>
+  <div class="list-group">
+  ${show.production_companies.map((company) => `<span>${company.name}</span>`).join(', ')}
+  </div>
+</div>`;
+document.querySelector('#show-details').appendChild(div);
+
+}
+
+//Display Slider Movies
+async function displaySlider(){
+  const { results }= await fetchAPIData('movie/now_playing');
+  results.forEach((movie)=>{
+    const div = document.createElement('div');
+    div.classList.add('swiper-slide')
+    div.innerHTML = `
+            <a href="movie-details.html?id=${movie.id}">
+              <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}" />
+            </a>
+            <h4 class="swiper-rating">
+              <i class="fas fa-star text-secondary"></i> ${movie.vote_average} / 10
+            </h4>
+    `;
+    document.querySelector('.swiper-wrapper').appendChild(div)
+
+    initSwiper();
+  })
+}
+
+function initSwiper(){
+  const swiper = new Swiper('.swiper', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    freeMode: true,
+    loop: true,
+    autoplay: {
+      delay:4000,
+      disableOnInteraction: false
+    },
+    breakpoints: {
+      500: {
+        slidesPerView: 2
+      },
+      700: {
+        slidesPerView: 3
+      },
+      1200: {
+        slidesPerView: 4
+      }
+    }
+  })
+}
+
 
 
 //Highlight Active Link
@@ -174,12 +304,13 @@ function init (){
         case '/' :
         case '/index.html' :
             displayPopularMovies();
+            displaySlider()
             break;
         case '/shows.html' :
             displayPopularShows();            
             break;   
         case '/tv-details.html' :
-            console.log('Tv Details');
+          displayShowDetails();
             break;
         case '/movie-details.html' :
             displayMovieDetails();
